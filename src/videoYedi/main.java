54cc177ef1712af1d181491e3d4a3f145f806d2f
gulci <gulci@LAPTOP-KPNR185M>
2022-09-21 17:14:44 +0300

    commityeni

---
+++ src/videoYedi/main.java
@@ -0,0 +1,17 @@
+package videoYedi;
+
+public class main {
+
+	public static void main(String[] args) {
+		int sayi=2;
+		sayi=13;
+		System.out.println(sayi);
+		char karakter='a';
+		
+//long ta tutarsak int in 2 katı bellek tutarız
+//default olarak ondalıklı sayıların veri tipi double dır
+//aslında char da karakter tuttuğu halde arka planda ASCII kod sayesinde verileri tuttuğu için sayısal bir veri tipidir.	
+	
+	}
+
+}
